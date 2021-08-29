528f87dd58a96fd7a775bc692b82b86059e94114
bmzhanglei <115097376@qq.com>
2021-08-29 17:35:31 +0800

    测试githubaction

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    outputDir:"dist",
-    publicPath:process.env.NODE_ENV==="production"? "/github-action2/":"/"
+    outputDir:"./dist",
+    publicPath:process.env.NODE_ENV!=="production"?"/": "/github-action2/"
 }
